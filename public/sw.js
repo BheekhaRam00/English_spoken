b408325c39c4e75bd690eef3f5e11d2dd18351d3
BheekhaRam00 <brsuthar2000@gmail.com>
2026-05-20 16:35:41 +0530

    Add service worker for caching static assets

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "fluentpro-v1";
+
+const STATIC_ASSETS = [
+  "/",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => {
+          return caches.match("/");
+        })
+      );
+    })
+  );
+});
