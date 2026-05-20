77589c650e1a65684f93b37535da6ad1a2f19764
BheekhaRam00 <brsuthar2000@gmail.com>
2026-05-20 19:31:17 +0530

    Add Providers component for context providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+
+import {
+  registerServiceWorker,
+  initializePWAInstallPrompt
+} from "@/lib/pwa";
+
+import AppContextProvider from "@/context/AppContext";
+
+import ConversationContextProvider from "@/context/ConversationContext";
+
+import SettingsContextProvider from "@/context/SettingsContext";
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+export default function Providers({
+  children
+}: ProvidersProps) {
+  useEffect(() => {
+    registerServiceWorker();
+
+    initializePWAInstallPrompt();
+  }, []);
+
+  return (
+    <SettingsContextProvider>
+      <AppContextProvider>
+        <ConversationContextProvider>
+          {children}
+        </ConversationContextProvider>
+      </AppContextProvider>
+    </SettingsContextProvider>
+  );
+}
